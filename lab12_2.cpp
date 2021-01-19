87ffdb87329f0575002e2939ca0e28173ed25d6f
fordsaranpong <forddbkzero@gmail.com>
2021-01-19 19:54:58 +0700

    lab.12

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -17,7 +17,24 @@ int main()
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
+       updateImage(image,s,x,y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
+}
+void showImage(const bool image[N][M]){
+int i,j;
+
+for(i=0;i<=31;i++){
+ for(j=0;j<=71;j++){
+ 	if(i==0||i==31)cout<<"-"; 
+	else if(j==71||j==0)cout<<"|";
+	else if(image[i][j]==1)cout<<"*";	
+	else cout<<" ";
+ }cout<<"\n";	
+}
+}
+void updateImage(bool [N][M],int s,int x,int y){
+
+
+
 }
